7a78e4472bbc14791b2178ea90124fda3a324c98
Anatoliy Pronin <piton.pronin@gmail.com>
2021-12-03 09:50:21 +0400

    add about page

--- app/controllers/web/about_controller.rb
+++ app/controllers/web/about_controller.rb
@@ -1,4 +1,5 @@
 class Web::AboutController < Web::ApplicationController
   def index
+    add_breadcrumbs(about_index_path, 'О банке')
   end
 end
--- app/controllers/web/knowledges_controller.rb
+++ app/controllers/web/knowledges_controller.rb
@@ -1,4 +1,5 @@
 class Web::KnowledgesController < Web::ApplicationController
   def index
+    add_breadcrumbs(knowledges_path, 'База знаний')
   end
 end
